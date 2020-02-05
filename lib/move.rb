c7b586989a4ca9c14e718318df5d29a3b533dab9
Andrew Persad <akpersad@gmail.com>
2020-02-05 02:28:19 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +9,22 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(arg)
+    if !!(arg =~ /\A[-+]?[0-9]+\z/) || arg == 0
+        arg = arg.to_i
+        if arg > 0
+            arg = arg - 1
+        else
+            arg
+        end
+    else
+        arg = -1
+    end
+end
+
+
+
+def move (board, position, choice = "X")
+  board[position] = choice
+  return board
+end
